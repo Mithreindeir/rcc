3047ca9dbbb5920dfc3512817f10711ad31ffba2
mithreindeir <mithreindeir@gmail.com>
2018-02-18 13:50:34 -0500

    Makes build dir

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 lex -o src/tokens.c src/tokens.lex
 bison -v --defines=include/parser.h --output=src/parser.c src/parser.y
+mkdir build
 gcc -o build/rcc src/parser.c src/tokens.c src/ast.c src/typecheck.c src/irgen.c src/quad.c src/symtable.c src/main.c -lfl -ggdb3
